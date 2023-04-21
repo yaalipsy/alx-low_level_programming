e22071706082a986bad274dae0ec029291d00543
yaalipsy <yaalipsy@github.com>
2023-04-21 15:47:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include < stdio.h
+#include < stdio.h >
 /**
  * main - {Programme that uses the puts function to print a line}
  * Retun: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multin=lingual puzzel");
-return (0);
+	put("\"Programming is like building a multin=lingual puzzel");
+	return (0);
 }
