5133f46f76a507f9b2793490d4d15f7ad6f55ab3
yaalipsy <yaalipsy@github.com>
2023-04-22 19:37:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  *main - A programme that prints a line using the pritf function
- *
- *Return : 0
+ *Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
