2078668eba4f840639dd06e3785c4d9579a2a553
yaalipsy <yaalipsy@github.com>
2023-04-22 18:07:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programme that prints using the puts function
+ * Return : 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
