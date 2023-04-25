f1a55335537807a21ddb37d439e1305a95155d99
yaalipsy <yaalipsy@github.com>
2023-04-25 08:36:32 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase and
+ * followed by a new line
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a', ch <= 'z', ch++)
+	{
+	putchar(ch);
+	}
+	putchar("\n');
+	return (0);
+}
