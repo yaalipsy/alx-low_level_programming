f85cdfd1687ee4422aac2b4c6e387dfa7776930a
yaalipsy <yaalipsy@github.com>
2023-04-25 11:01:10 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers up to 10
+ * starting from 0, followed ny a new line
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	print("\n")'
+	return (0);
+}
