3351bebed48b2a216b7520aa12cbab910efba98f
yaalipsy <yaalipsy@github.com>
2023-04-25 09:19:37 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - Prints alphabets in both upper and lower case
+ * followed by a new line
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch ch++)
+		putchar(ch);
+	putchar("\n');
+	return (0);
+}
