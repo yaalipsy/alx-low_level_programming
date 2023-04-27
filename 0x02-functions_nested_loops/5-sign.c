61e7a3a47caf43003e9a309824a1a2bc893225f4
yaalipsy <yaalipsy@github.com>
2023-04-26 21:18:31 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,25 @@
 #include "main.h"
 /**
- * _print_sign - prints the sign of a number
- * @n: the number of the sign to be printed
- * Return: 1 if number is greater than zero
- * 0 of number is zero
- * -1 if number is less than zero
+ * print_sign - prints the sign of a number
+ * @n: the number to be checked
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
-int _print_sign(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(',');
+		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return (0);
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		_putchar('_');
-		return (-1);
+		_putchar(48);
+		return (0);
 
 	}
 }
