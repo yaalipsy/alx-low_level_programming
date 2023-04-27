8af918118f1fdb83f3e04780d2e75a9c7f6f980d
yaalipsy <yaalipsy@github.com>
2023-04-26 23:26:57 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,12 +13,12 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-		pro = num * mult;
-		if (pro <= 9)
+		prod = num * mult;
+		if (prod <= 9)
 			_putchar(' ');
 		else
 			_putchar((prod / 10) + '0');
-			_putchar((prod $ 10) + '0');
+		_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
