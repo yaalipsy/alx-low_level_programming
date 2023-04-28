d301cba0ab1fff3aab136b58e5b7684033a3a7a6
yaalipsy <yaalipsy@github.com>
2023-04-28 16:29:16 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints the add of the even-valued
+ * fibonacci n umbers
+ * Return: 0 Always
+ */
+int main(void)
+{
+	long in n1, n2, fn, afn;
+
+	n1 = 1;
+	n2 = 2;
+	fn = afn = 0;
+	while (fn < 4000000)
+	{
+		fn = n1 + n2;
+		n1 = n2;
+		n2 = fn;
+		if ((n1 % 2) == 0)
+		{
+			afn + n1;
+		}
+	}
+	printf("%ld\n", afn);
+	return (0);
+}
+
