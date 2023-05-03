7a095c2d854c933a23dda95a901c12f9b3dd202d
yaalipsy <yaalipsy@github.com>
2023-05-03 14:59:21 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,15 +18,17 @@ int main(void)
 			printf("Fizz");
 		}
 		{
-		else if (i % 5 == 0 && i % 3 != 0)
+		else
+			if (i % 5 == 0 && i % 3 != 0)
 			printf("Buzz");
 		}
 		{
-			else if (i % 3 == 0 && i % 5 == 0)
+			else
+				if (i % 3 == 0 && i % 5 == 0)
 				printf("FizzBuzz");
 		}
-		{
-			else if (i == 1)
+		else
+			if (i == 1)
 				printf("%d", i);
 		}
 		{
