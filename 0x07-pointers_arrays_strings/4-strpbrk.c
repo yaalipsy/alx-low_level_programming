0f8548c26fdd8b7600cd98d531e4a511a606042d
yaalipsy <yaalipsy@github.com>
2023-05-10 14:11:29 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - Entry poiyn
+ * @s: input
+ * @accept: input
+ * Return: Always 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
