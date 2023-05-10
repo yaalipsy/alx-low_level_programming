564d3ac4d77747c022853834eed16867195e411a
yaalipsy <yaalipsy@github.com>
2023-05-10 15:13:52 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,5 +17,9 @@ void print_diagsums(int *a, int size)
 	{
 		sum1 = sum1 + a[y * size + y];
 	}
+	for (y = size - 1; y >= 0; y--)
+	{
+		sum2 += a[y * size + (size - y - 1)];
+	}
 	printf("%d, %d\n", sum1, sum2);
 }
