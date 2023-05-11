1dc08c2a768b80c950bb8f5f8ae78810e46de4ad
yaalipsy <yaalipsy@github.com>
2023-05-11 08:35:24 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s21 != '\0' && *s2 == '*')
+		{
+			return (wildcmps(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	return (0);
+}
