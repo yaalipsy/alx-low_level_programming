fd68ca91bbdf6a9a6c2bc464750686f01c29a9e4
yaalipsy <yaalipsy@github.com>
2023-05-13 17:35:05 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * follow by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
