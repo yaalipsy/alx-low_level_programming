98a34a246ad31013db458d17d836db60bee8e918
yaalipsy <yaalipsy@github.com>
2023-05-16 21:23:13 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,10 +17,9 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	s2 = "";
 
 	i = ci = 0;
-	while (s1[1] != '\0')
+	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
