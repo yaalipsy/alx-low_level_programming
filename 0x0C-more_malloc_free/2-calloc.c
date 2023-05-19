af12adda8a4200e8fa95d6d3713673349ab29c5a
yaalipsy <yaalipsy@github.com>
2023-05-18 17:19:14 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,7 @@
  * @n: number of times to copy b
  * Return: pointer to the memory area s
  */
-char * _memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
