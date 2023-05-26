93e992e1c31879d0d1f382e9a8019e453b4df66f
yaalipsy <yaalipsy@github.com>
2023-05-26 09:33:47 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,19 +6,17 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 	int bytes, i;
 	char *arr;
 
-
 	if (argc != 2)
 	{
 		printf("Error\n");
-			exit(1);
+		exit(1);
 	}
 
 	bytes = atoi(argv[1]);
@@ -38,7 +36,7 @@ int main(int argc, char *argv[])
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx", arr[i]);
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
