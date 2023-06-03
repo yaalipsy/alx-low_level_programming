712fdd8fa8a660cb381917b78887132b511dda3c
yaalipsy <yaalipsy@github.com>
2023-06-02 18:36:00 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,7 +4,8 @@
 
 /**
  * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list'@str: new string to add in the node
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  * Return: the address of the new element, or NULL if it fails
  */
 
