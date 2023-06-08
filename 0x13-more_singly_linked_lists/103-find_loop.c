802a52bd8cc91615c02bc647965c452f89c812e4
yaalipsy <yaalipsy@github.com>
2023-06-08 01:55:37 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -12,31 +12,27 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *tortoise, *hare;
 
-
 	if (head == NULL || head->next == NULL)
 		return (NULL);
 
-		tortoise = head->next;
-		hare = (head->next)->next;
+	tortoise = head->next;
+	hare = (head->next)->next;
 
-		while (hare)
+	while (hare)
+	{
+		if (tortoise == hare)
 		{
-			if (tortoise == hare)
+			tortoise = head;
+
+			while (tortoise != hare)
 			{
-				tortoise = head;
-
-				while (tortoise != hare)
-				{
-					tortoise = tortoise->next;
-					hare = hare->next;
-				}
-
-				return (tortoise);
+				tortoise = tortoise->next;
+				hare = hare->next;
 			}
-
-			tortoise = tortoise->next;
-			hare = (hare->next)->next;
+			return (tortoise);
 		}
-
-		return (NULL);
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (NULL);
 }
