cf3bdf6baeacb676503d95d860d40b06821c098e
yaalipsy <yaalipsy@github.com>
2023-06-09 07:02:05 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * clear_bit -Set the value of a bit to 0 at a given index
+ * @n: pointer to decimal
+ * @index: index position
+ * Return: 1 if it worked, -1 if error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+	unsigned int set;
+
+	if (index > 64)
+		return (-1);
+	set = index;
+	for (i = 1; set > 0; i *= 2, set--);
+
+	if ((*n >> index) & 1)
+		*n -= i;
+
+	return (1);
+}
