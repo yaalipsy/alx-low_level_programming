6ed3d3d94dd21c7a3755df1f31516b015bd4b9c1
yaalipsy <yaalipsy@github.com>
2023-06-09 07:26:18 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - Flip bits to convert one number to aother
+ * @n: first number
+ * @m: second number to convert to
+ * Return: number of bits needed to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i;
+	int counter;
+
+	i = n ^ m;
+	counter = 0;
+
+	while (1)
+	{
+		counter++;
+		i &= (i - 1);
+	}
+
+	return (counter);
+}
