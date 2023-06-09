79ec1b7bf64b1bd799b7396bb2b76b101b74e0a9
yaalipsy <yaalipsy@github.com>
2023-06-09 08:04:41 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - Checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int i = 1;
+
+
+	if (*(char *)&i == 1)
+		return (1);
+
+	else
+		return (0);
+}
