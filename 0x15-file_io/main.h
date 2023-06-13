b176fccf0a8f9f5cf357d7f26e9632d41d70ff41
yaalipsy <yaalipsy@github.com>
2023-06-12 17:09:14 -0700

    1-create_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int creat_flie(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 
